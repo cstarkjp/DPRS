0969eb2caf35bfcda10bf94d928a982e9937a2b4
cstarkjp <cstarkjp@gmail.com>
2026-03-07 17:01:21 +0800

    Run rs sim() from pip pkg python

--- python/dprs/pydp.py
+++ python/dprs/pydp.py
@@ -1,1 +1,10 @@
-print("Directed percolation and other models")
+from dprs import sim
+
+def demo() -> None:
+    print(sim)
+
+    n_x = 1_000
+    n_y = n_x
+    n_iterations = 200
+
+    sim.dp(n_x, n_y, n_iterations)
