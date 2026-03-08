8eca226cb111ac6d79d060f8d50321d0fa280496
cstarkjp <cstarkjp@gmail.com>
2026-03-08 22:50:16 +0800

    Put back "slow_factor" for serial timing

--- src/sim_life/monitor.rs
+++ src/sim_life/monitor.rs
@@ -7,14 +7,13 @@ pub fn monitor(
     n_x: usize, n_y: usize, n_iterations: usize, slow_factor: usize,
 ) -> (f64, Vec<bool>) {
     let grid = LatticeModel2D::initialize(n_x, n_y).randomize();
-    // let lattice = compute(grid, n_iterations/slow_factor);
     let n_threads = 16;
     let pool = rayon::ThreadPoolBuilder::new()
         .num_threads(n_threads)
         .build()
         .unwrap();
     let time = Instant::now();
-    let lattice = pool.install(|| compute(grid, n_iterations));
+    let lattice = pool.install(|| compute(grid, n_iterations/slow_factor));
     let duration = time.elapsed().as_secs_f64() * (slow_factor as f64);
 
     (duration, lattice)
