821668d7ec6b2e12493cedbac9224a174c148cfc
cstarkjp <cstarkjp@gmail.com>
2026-03-08 22:48:33 +0800

    GJS mods

--- src/sim_life/compute.rs
+++ src/sim_life/compute.rs
@@ -2,9 +2,8 @@ use crate::sim_life::LatticeModel2D;
 
 /// Run a simulation for n_iterations using serial processing.
 pub fn compute_serial(
-    lattice_model: LatticeModel2D, n_iterations: usize,
+    mut lattice_model: LatticeModel2D, n_iterations: usize,
 ) -> Vec<bool> {
-    let mut lattice_model = lattice_model;
     for _ in 0..n_iterations {
         lattice_model = lattice_model.next_iteration_serial();
     }
@@ -14,9 +13,8 @@ pub fn compute_serial(
 
 /// Run a simulation for n_iterations using parallel processing.
 pub fn compute_parallel(
-    lattice_model: LatticeModel2D, n_iterations: usize,
+    mut lattice_model: LatticeModel2D, n_iterations: usize,
 ) -> Vec<bool> {
-    let mut lattice_model = lattice_model;
     for _ in 0..n_iterations {
         lattice_model = lattice_model.next_iteration_parallel();
     }
--- src/sim_life/monitor.rs
+++ src/sim_life/monitor.rs
@@ -6,9 +6,15 @@ pub fn monitor(
     compute: fn(LatticeModel2D, usize) -> Vec<bool>, 
     n_x: usize, n_y: usize, n_iterations: usize, slow_factor: usize,
 ) -> (f64, Vec<bool>) {
-    let time = Instant::now();
     let grid = LatticeModel2D::initialize(n_x, n_y).randomize();
-    let lattice = compute(grid, n_iterations/slow_factor);
+    // let lattice = compute(grid, n_iterations/slow_factor);
+    let n_threads = 16;
+    let pool = rayon::ThreadPoolBuilder::new()
+        .num_threads(n_threads)
+        .build()
+        .unwrap();
+    let time = Instant::now();
+    let lattice = pool.install(|| compute(grid, n_iterations));
     let duration = time.elapsed().as_secs_f64() * (slow_factor as f64);
 
     (duration, lattice)
