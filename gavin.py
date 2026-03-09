c71e7098df98578852457ea2ddb00c278ab8141b
Gavin Stark <atthecodeface@gmail.com>
2026-03-09 12:30:21 +0000

    Tweaked gavin.py to work with Colin's version

--- gavin.py
+++ gavin.py
@@ -6,7 +6,7 @@ import sys
 this_dir = pathlib.Path(__file__).parent.resolve()
 sys.path.append(this_dir.joinpath("target", "release").__str__())
 
-import life
+import sim
 
-os.environ["RAYON_NUM_THREADS"] = "2"
-life.sim(10000, 10000, 100)
+# x, y, iterations, slow factor, threads
+sim.life(1000, 1000, 10, 1, 10)
