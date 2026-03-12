e1997ff0b323ec009c493376f04a0c3f50022d90
Gavin Stark <atthecodeface@gmail.com>
2026-03-12 15:11:33 +0000

    Tweaks to gavin.py to cope with Rust-defined enums

--- gavin.py
+++ gavin.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-import os
 import pathlib
 import sys
+from enum import Enum
 
 this_dir = pathlib.Path(__file__).parent.resolve()
 sys.path.append(this_dir.joinpath("target", "release").__str__())
@@ -10,12 +10,10 @@ import dprs as sim
 
 
 class Parameters:
-    # dim: Dimension::D1,
-    # processing: Processing::ParallelChunked,
-    dim: int = 0
-    processing: int = 2
-    n_x: int = 1
-    n_y: int = 1
+    dim: sim.Dimension = sim.Dimension.D2
+    processing: sim.Processing = sim.Processing.ParallelChunked
+    n_x: int = 10000
+    n_y: int = 10000
     n_z: int = 1
     p: float = 0.5
     n_iterations: int = 1
@@ -26,24 +24,5 @@ class Parameters:
 
 
 print(f"\n{sim}\n")
-
+# help(sim)
 _ = sim.life(Parameters())
-
-# num_iter = 10
-# thread_counts = range(1, 11)
-# sizes = [1000, 2000, 3000, 4000]
-# for size in sizes:
-#    ns = life.serial(size, size, num_iter)[1]
-#    print(f"{size}: {ns / (size * size)}ns per cell for {num_iter} iterations serially")
-#    for num_threads in thread_counts:
-#        ns = life.parallel(num_threads, size, size, num_iter)[1]
-#        print(
-#            f"{size}: {ns / (size * size)}ns per cell for {num_iter} iterations with {num_threads} threads"
-#        )
-#    for num_threads in thread_counts:
-#        ns = life.parallel_chunked(num_threads, size, size, num_iter)[1]
-#        print(
-#            f"{size}: {ns / (size * size)}ns per cell for {num_iter} iterations with {num_threads} threads using chunking"
-#        )
-#        pass
-#    pass
