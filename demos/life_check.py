9e4d029c77021e403d43e1f7124693d94be4c824
Gavin Stark <atthecodeface@gmail.com>
2026-03-12 12:41:20 +0000

    Changed python files to use Parameters not **kwargs

--- demos/life_check.py
+++ demos/life_check.py
@@ -2,16 +2,31 @@ from dprs import sim
 
 print(f"\n{sim}\n")
 
-kwargs = dict(
-    n_x = 10_000,
-    n_y = 10_000,
-    # n_z = 1,
-    p = 0.5,
-    n_iterations = 50,
-    sample_rate = 10,
-    n_threads = 8,
-    serial_skip = 10,
-    do_buffering = 0, # currently booleans not correctly parsed
-)
 
-_ = sim.life(**kwargs)
+class Parameters:
+    # dim: Dimension::D1,
+    # processing: Processing::ParallelChunked,
+    dim: int = 0
+    processing: int = 2
+    n_x: int = 1
+    n_y: int = 1
+    n_z: int = 1
+    p: float = 0.5
+    n_iterations: int = 1
+    sample_rate: int = 10
+    n_threads: int = 1
+    serial_skip: int = 1
+    do_buffering: bool = True
+
+
+parameters = Parameters()
+parameters.n_x = 10_000
+parameters.n_y = 10_000
+parameters.p = 0.5
+parameters.n_iterations = 50
+parameters.sample_rate = 10
+parameters.n_threads = 8
+parameters.serial_skip = 10
+parameters.do_buffering = False
+
+_ = sim.life(parameters)
--- python/dprs/pylife.py
+++ python/dprs/pylife.py
@@ -1,12 +1,30 @@
 from dprs import sim
 
+
+class Parameters:
+    # dim: Dimension::D1,
+    # processing: Processing::ParallelChunked,
+    dim: int = 0
+    processing: int = 2
+    n_x: int = 1
+    n_y: int = 1
+    n_z: int = 1
+    p: float = 0.5
+    n_iterations: int = 1
+    sample_rate: int = 10
+    n_threads: int = 1
+    serial_skip: int = 1
+    do_buffering: bool = True
+
+
 def demo() -> None:
     print(sim)
 
-    n_x = 10_000
-    n_y = 10_000
-    n_iterations = 100
-    slow_factor = 10
-    n_threads = 16
+    parameters = Parameters()
+    parameters.n_x = 10_000
+    parameters.n_y = 10_000
+    parameters.n_iterations = 100
+    # ?? parameters.slow_factor = 10
+    parameters.n_threads = 16
 
-    print(sim.life(n_x, n_y, n_iterations, slow_factor, n_threads,))
+    print(sim.life(parameters))
