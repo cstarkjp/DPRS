50c1f2daee119f301ff4595c533b4875d47c7ca9
Gavin Stark <atthecodeface@gmail.com>
2026-03-12 12:26:41 +0000

    Changed to gavin.py to support FromPyObject testing

--- gavin.py
+++ gavin.py
@@ -8,6 +8,31 @@ sys.path.append(this_dir.joinpath("target", "release").__str__())
 
 import dprs as sim
 
+
+class MyParameters:
+    # pub dim: MyDimension,
+    dim: int = 0
+    processing: int = 0
+    n_x: int = 1
+    n_y: int = 1
+    n_z: int = 1
+    p: float = 0.5
+    n_iterations: int = 10000
+    sample_rate: int = 10000
+    # pub processing: MyProcessing,
+    n_threads: int = 4
+    serial_skip: int = 10
+    do_buffering: bool = False
+
+
+class MyObject:
+    msg: str = "Default message"
+    list = [1, 2, 3, 4]
+
+
+sim.blob(MyObject())
+sim.blah(MyParameters())
+
 print(f"\n{sim}\n")
 
 kwargs = dict(
