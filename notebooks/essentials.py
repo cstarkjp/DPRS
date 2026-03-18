d0b7db2ca0da8e8617ad398b0f1f4c97f5c5aeb7
cstarkjp <cstarkjp@gmail.com>
2026-03-18 21:21:26 +0800

    Update nbs accordingly

--- notebooks/essentials.py
+++ notebooks/essentials.py
@@ -7,4 +7,4 @@ from numpy.typing import NDArray
 from dprs import sim
 from dprs.viz import Viz
 from dprs.file import create_directories, export_plots
-from dprs.utils import make_name, make_title
+from dprs.utils import make_name, make_title, DP, dp_state as state
