ae432f2998541f299a94970a6a50b0799ebd7623
Gavin Stark <atthecodeface@gmail.com>
2026-03-19 12:41:12 +0000

    Parallel iteration sim test working

--- src/tests/nbrhood_3d.rs
+++ src/tests/nbrhood_3d.rs
@@ -30,14 +30,28 @@ impl CellModel3D for Model3D {
     }
 }
 
-fn value(parameters: &Parameters, i: usize, opt_dxyz: Option<(i8, i8, i8)>) -> isize {
+fn value(
+    parameters: &Parameters,
+    i: usize,
+    opt_dxyz: Option<(i8, i8, i8)>,
+    zero_edge: bool,
+) -> isize {
     let mut x = (i % parameters.n_x) as isize;
-    let mut y = (i / parameters.n_x % parameters.n_y) as isize;
-    let mut z = (i / parameters.n_x / parameters.n_y) as isize;
+    let mut y = ((i / parameters.n_x) % parameters.n_y) as isize;
+    let mut z = ((i / parameters.n_x) / parameters.n_y) as isize;
     if let Some(dxyz) = opt_dxyz {
         x += dxyz.0 as isize;
         y += dxyz.1 as isize;
         z += dxyz.2 as isize;
+        if zero_edge {
+            // If n_x is 4 then we expect only 1 & 2 to be non-zero, i.e. 1..3
+            if !(1..(parameters.n_x as isize - 1)).contains(&x)
+                || !(1..(parameters.n_y as isize - 1)).contains(&y)
+                || !(1..(parameters.n_z as isize - 1)).contains(&z)
+            {
+                return 0;
+            }
+        }
     };
     10 + x + y * 7 + z * 13
 }
@@ -55,7 +69,7 @@ fn test_dp() {
 
     let mut lm = LatticeModel3D::new(Model3D(), n_x, n_y, n_z, (0, 0), (1, 1), (2, 2));
     for (i, l) in lm.lattice_mut().iter_mut().enumerate() {
-        *l = value(&parameters, i, None);
+        *l = value(&parameters, i, None, false);
     }
 
     for (mut x, y, z) in [(1, 1, 1)] {
@@ -72,7 +86,7 @@ fn test_dp() {
                     for dz in (-1)..2 {
                         assert_eq!(
                             nbrhood_of_xyz[((dx + 1) as u8, (dy + 1) as u8, (dz + 1) as u8)],
-                            value(&parameters, i, Some((dx, dy, dz))),
+                            value(&parameters, i, Some((dx, dy, dz)), false),
                             "Contents for {x},{y},{z} : {i} : {dx},{dy},{dz} should match"
                         );
                     }
@@ -92,29 +106,29 @@ fn test_dp() {
 fn test_sim() {
     let mut parameters = Parameters::default();
     parameters.dim = Dimension::D3;
-    parameters.n_x = 4;
-    parameters.n_y = 5;
-    parameters.n_z = 6;
-    parameters.n_iterations = 1;
-    parameters.sample_rate = 1;
+    parameters.n_x = 10;
+    parameters.n_y = 15;
+    parameters.n_z = 20;
+    parameters.n_iterations = 40;
+    parameters.sample_rate = 3;
     parameters.seed = 1;
     parameters.axis_topology_x = Topology::Open;
     parameters.axis_topology_y = Topology::Open;
     parameters.axis_topology_z = Topology::Open;
-    parameters.axis_bcs_x = (BoundaryCondition::Floating, BoundaryCondition::Floating);
-    parameters.axis_bcs_y = (BoundaryCondition::Floating, BoundaryCondition::Floating);
-    parameters.axis_bcs_z = (BoundaryCondition::Floating, BoundaryCondition::Floating);
+    parameters.axis_bcs_x = (BoundaryCondition::Pinned, BoundaryCondition::Pinned);
+    parameters.axis_bcs_y = (BoundaryCondition::Pinned, BoundaryCondition::Pinned);
+    parameters.axis_bcs_z = (BoundaryCondition::Pinned, BoundaryCondition::Pinned);
     let mut lm = LatticeModel3D::new(
         Model3D(),
         parameters.n_x,
         parameters.n_y,
         parameters.n_z,
+        (0, 0), // End values have to match 'value'
         (0, 0),
-        (1, 1),
-        (2, 2),
+        (0, 0),
     );
     for (i, l) in lm.lattice_mut().iter_mut().enumerate() {
-        *l = value(&parameters, i, None);
+        *l = value(&parameters, i, None, false);
     }
     let (n_lattices, lattices, tracking) = crate::dp::simulation_3d(
         lm,
@@ -127,17 +141,32 @@ fn test_sim() {
     assert_eq!(
         &lattices[0],
         &(0..parameters.n_x * parameters.n_y * parameters.n_z)
-            .map(|i| value(&parameters, i, None))
+            .map(|i| value(&parameters, i, Some((0, 0, 0)), true))
             .collect::<Vec<_>>()
     );
-    for x in 1..parameters.n_x - 1 {
-        for y in 1..parameters.n_y - 1 {
-            for z in 1..parameters.n_z - 1 {
-                let c = lattices[0][(z * parameters.n_y + y) * parameters.n_x + x];
-                eprintln!("{x}, {y}, {z}, {c}");
+    for (iter, l) in lattices.iter().enumerate().skip(1) {
+        for (i, c) in l.iter().enumerate() {
+            // m is 1 for max of nbrhod (+-1); 2 for max of nbrhood(+-2); etc
+            let m = (iter * parameters.sample_rate) as i8;
+            if value(&parameters, i, Some((0, 0, 0)), true) == 0 {
+                assert_eq!(
+                    *c, 0,
+                    "Value at {i} for iteration {iter} should be edge of 0"
+                );
+            } else {
+                let max = ((-m)..=m)
+                    .flat_map(|dx| ((-m)..=m).map(move |dy| (dx, dy)))
+                    .flat_map(|dxy| ((-m)..=m).map(move |dz| (dxy, dz)))
+                    .fold(0, |acc, ((dx, dy), dz)| {
+                        acc.max(value(&parameters, i, Some((dx, dy, dz)), true))
+                    });
+                // eprintln!("{i}:{max}:{c}");
+                assert_eq!(
+                    *c, max,
+                    "Value at {i} (non-edge) for iteration {iter} incorrect"
+                );
             }
         }
     }
-    dbg!(&lattices[1]);
-    assert!(false, "Force failure");
+    // assert!(false, "Force failure");
 }
