148097fad47a84bdd4d966b6f239f454c9a6f22b
cstarkjp <cstarkjp@gmail.com>
2026-04-18 12:20:25 +0800

    Exploring behavior of current 1d model B

--- dprs_core/src/dk/model_bedload_b_1d.rs
+++ dprs_core/src/dk/model_bedload_b_1d.rs
@@ -54,9 +54,9 @@ impl GrowthModel<Cell1D> for ModelBedloadB1D {
         rng: &mut R,
         nbrhood: &[bool; 3],
     ) -> DualState {
-        let is_upstream_occupied = nbrhood[0];
+        let is_upstream_occupied   = nbrhood[0];
+        let is_here_occupied       = nbrhood[1];
         let is_downstream_occupied = nbrhood[2];
-        let is_here_occupied = nbrhood[1];
         let do_survive = ((is_here_occupied | is_upstream_occupied) & rng.random_bool(self.p_1))
             | ((is_here_occupied & is_upstream_occupied) & rng.random_bool(self.p_2))
             | ((is_here_occupied & is_downstream_occupied) & rng.random_bool(self.p_2*self.bias))
