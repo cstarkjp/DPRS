6ecb69242ace891a47aa31f209ea28c31301f631
cstarkjp <cstarkjp@gmail.com>
2026-04-19 17:19:33 +0800

    Deployed 4f82246 with MkDocs version: 1.6.1

--- live-demos/pkg/dprs_wasm.d.ts
+++ live-demos/pkg/dprs_wasm.d.ts
@@ -60,12 +60,6 @@ export interface InitOutput {
     readonly __wbg_set_topobc_periodic: (a: number, b: number) => void;
     readonly __wbg_topobc_free: (a: number, b: number) => void;
     readonly topobc_new: () => number;
-    readonly __wbg_simulation_free: (a: number, b: number) => void;
-    readonly simulation_new: (a: number) => number;
-    readonly simulation_parameters: (a: number) => number;
-    readonly simulation_result: (a: number, b: number) => [number, number];
-    readonly simulation_set_parameters: (a: number, b: number) => void;
-    readonly simulation_simulate: (a: number, b: number, c: number) => [number, number];
     readonly __wbg_parameters_free: (a: number, b: number) => void;
     readonly parameters_initial_condition: (a: number) => [number, number];
     readonly parameters_n_iterations: (a: number) => number;
@@ -93,6 +87,12 @@ export interface InitOutput {
     readonly parameters_set_topo_bc_z: (a: number, b: number) => void;
     readonly parameters_sim_dimension: (a: number) => number;
     readonly parameters_topo_bc_x: (a: number) => number;
+    readonly __wbg_simulation_free: (a: number, b: number) => void;
+    readonly simulation_new: (a: number) => number;
+    readonly simulation_parameters: (a: number) => number;
+    readonly simulation_result: (a: number, b: number) => [number, number];
+    readonly simulation_set_parameters: (a: number, b: number) => void;
+    readonly simulation_simulate: (a: number, b: number, c: number) => [number, number];
     readonly __wbindgen_externrefs: WebAssembly.Table;
     readonly __wbindgen_free: (a: number, b: number, c: number) => void;
     readonly __wbindgen_malloc: (a: number, b: number) => number;
