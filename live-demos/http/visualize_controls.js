5de5ee81fb2b55b43b3dc8532da9d9c8b860dc34
cstarkjp <cstarkjp@gmail.com>
2026-04-30 12:34:59 +0800

    Deployed cb63e1e with MkDocs version: 1.6.1

--- live-demos/http/visualize_controls.js
+++ live-demos/http/visualize_controls.js
@@ -49,18 +49,27 @@ export class VisualizeControls {
         tr_playback.add_ele("td").add_input_button("⏪︎", () => {
             this.parent.playback_simulation(-60);
         }, { classes: "controls playback reverse" });
-        tr_playback.add_ele("td").add_input_button("⏸", () => {
+        tr_playback.add_ele("td").add_input_button(
+        // "⏸", // unicode version U+23F8
+        "⏸", // emoji version, &#9208, possibly
+        () => {
             this.parent.playback_simulation(0);
         }, { classes: "controls playback stop" });
-        tr_playback.add_ele("td").add_input_button("⏩︎", () => {
+        tr_playback.add_ele("td").add_input_button(
+        // "⏩︎",
+        "⏵", () => {
             this.parent.playback_simulation(60);
         }, { classes: "controls playback play" });
-        tr_playback.add_ele("td").add_input_button("⏴", () => {
+        tr_playback.add_ele("td").add_input_button(
+        // "⏴",
+        "➖", () => {
             // Step backward by one iteration: replaces slow reverse playback
             // this.parent.playback_simulation(-10);
             this.parent.decrement_slice();
         }, { classes: "controls playback decrement" });
-        tr_playback.add_ele("td").add_input_button("⏵", () => {
+        tr_playback.add_ele("td").add_input_button(
+        // "⏵",
+        "➕", () => {
             // Step forward by one iteration: replaces slow forward playback
             // this.parent.playback_simulation(10);
             this.parent.increment_slice();
